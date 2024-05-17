52d7d89bcec5d0b8e5089485075b466d815241fd
pedrozvxlindo <151748032+pedrozvxlindo@users.noreply.github.com>
2024-05-17 07:47:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
 for(let i=0;i<botoes.length;i++){
     botoes[i].onclick=function(){
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-06-17T00:00:00");
+contadores[0].textContent = tempoObjetivo1; 
